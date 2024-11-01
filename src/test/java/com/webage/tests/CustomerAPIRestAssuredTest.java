624edf4939fed28b9dad5d740bf58ec78ab0db67
zacht9 <zachteselko@gmail.com>
2024-11-01 19:35:27 +0000

    Added tests (but didn't debug them :') )

---
+++ src/test/java/com/webage/tests/CustomerAPIRestAssuredTest.java
@@ -0,0 +1,126 @@
+package com.webage.tests;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.Test;
+
+import io.restassured.response.Response;
+
+public class CustomerAPIRestAssuredTest {
+	
+	@BeforeClass
+    public void setUp() {
+        baseURI = "http://localhost:8080/api";
+    }
+
+	
+	@Test
+    public void testGetAPIRequest() {
+        given()
+            .when()
+                .get("http://localhost:8080/api")
+            .then()
+                .statusCode(200);
+    }
+	
+	@Test
+    public void testGetAllCustomersRequest() {
+        given()
+            .when()
+                .get("http://localhost:8080/api/customers")
+            .then()
+                .statusCode(200);
+        
+    }
+	
+	@Test
+    public void testPostRequest() {
+        given()
+            .contentType("application/json")
+            .body("{\r\n"
+            		+ "        \"id\": 0,\r\n"
+            		+ "        \"name\": \"fox\",\r\n"
+            		+ "        \"password\": \"pass123\",\r\n"
+            		+ "        \"email\": \"fox@webage.com"
+            		+ "\"\r\n"
+            		+ "}")
+            
+        .when()
+            .post("http://localhost:8080/api/customers")
+        .then()
+            .statusCode(201)
+            .header("Location", notNullValue());    
+    }
+	
+	
+	@Test
+    public void testPutRequest() {
+        given()
+            .contentType("application/json")
+            .body("{\r\n"
+            		+ "        \"id\": 1,\r\n"
+            		+ "        \"name\": \"steve\",\r\n"		
+            		+ "        \"password\": \"steve123\",\r\n"		//password update
+            		+ "        \"email\": \"steve@webage.com"		//email update
+            		+ "\"\r\n"
+            		+ "}")
+            
+        .when()
+            .put("http://localhost:8080/api/customers/1")
+        .then()
+            .statusCode(200);    
+    }
+	
+	@Test
+    public void testStatusCodeHandling() {
+        given()
+            .when()
+                .get("http://localhost:8080/api/customers/1")
+            .then()
+                .statusCode(
+                    anyOf(
+                        is(200), // Success
+                        is(404), // Not Found
+                        is(500)  // Server Error
+                    )
+                );
+    }
+	
+	@Test
+    public void testRetrieveResponseBody() {
+        Response response =
+            given()
+            .when()
+                .get("http://localhost:8080/api/customers/1")
+            .then()
+                .statusCode(200)
+            .extract()
+                .response();
+        
+        String responseBody = response.getBody().asString();
+        System.out.println("Response Body: " + responseBody);
+    }
+	
+	@Test
+    public void testDeleteRequest() {
+            given()
+            .when()
+                .delete("http://localhost:8080/api/customers/2")
+            .then()
+                .statusCode(204);
+            
+    }
+	
+	@Test
+    public void testEvaluateJSONContent() {
+        given()
+            .when()
+                .get("http://localhost:8080/api/customers/1")
+            .then()
+                .statusCode(200)
+                .body("name", equalTo("steve"))
+                .body("email", equalTo("steve@webage.com"));
+    }
+}
